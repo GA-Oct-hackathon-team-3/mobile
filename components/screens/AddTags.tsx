838d93e66e4e64ae0bf22046dddde0def62415e1
Amber Taveras <taveras.amber@gmail.com>
2023-11-29 04:20:47 -0500

    extracts component to render in both add and edit tags

--- components/screens/AddTags.tsx
+++ components/screens/AddTags.tsx
@@ -1,100 +1,24 @@
-import { useLocalSearchParams, useNavigation, useRouter } from "expo-router";
-import React, { useEffect, useRef, useState } from "react";
-import {
-  ActivityIndicator,
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-  useWindowDimensions,
-} from "react-native";
-import { colors } from "../../constants/Theme";
-import * as friendsService from "../../utilities/friends-service";
+import { useLocalSearchParams } from "expo-router";
+import React, { useEffect, useState } from "react";
 import * as tagsService from "../../utilities/tags-service";
-import TitleBack from "../TitleBack";
-import { FontAwesome } from "@expo/vector-icons";
 import TagAdder from "../TagAdder";
 
 export default function AddTags() {
   const params = useLocalSearchParams();
-  const navigation = useNavigation();
-
-  const [searchTag, setSearchTag] = useState("");
-  const [addedTags, setAddedTags] = useState([]);
 
   const [tags, setTags] = useState([]);
   const [defaultTags, setDefaultTags] = useState([]);
-  const [suggestedTags, setSuggestedTags] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-  const scrollViewRef = useRef<ScrollView>();
-  const [friend, setFriend] = useState(null);
-  const searchTagLower = searchTag.toLowerCase();
-  const includesTag = addedTags.some(
-    (tag) => tag.toLowerCase() === searchTagLower
-  );
 
   const fetchTags = async () => {
-    const friendData = await friendsService.retrieveFriend(params.id);
-    setTags(friendData.tags)
-
-    console.log(friendData);
-
     const tagsData = await tagsService.getTags();
     setDefaultTags(tagsData);
-    console.log(tagsData);
-
-    setAddedTags(friendData.tags);
-    setFriend(friendData);
   };
 
   useEffect(() => {
     fetchTags();
   }, []);
 
-  useEffect(() => {}, [addedTags]);
-
-  const handleSearchSubmit = () => {
-    if (!addedTags.includes(searchTag) && searchTag !== "") {
-      setAddedTags((prev) => [...prev, searchTag]);
-      setSearchTag("");
-    }
-  };
-
-  function scrollViewSizeChanged(height) {
-    // y since we want to scroll vertically, use x and the width-value if you want to scroll horizontally
-    scrollViewRef.current?.scrollTo({ y: height, animated: true });
-  }
-
-  const handleSubmit = async () => {
-    setLoading(true);
-
-    try {
-      addedTags.forEach(async (tag) => {
-        await tagsService.addTag(friend._id, {
-          title: tag,
-          category: "Popular",
-        });
-      });
-    } catch (err) {}
-    setTimeout(() => {
-      setLoading(false);
-      navigation.dispatch({ type: "POP_TO_TOP" });
-    }, 3000);
-  };
-
-  const handleTagPress = (tag) => {
-    if (!addedTags.includes(tag)) {
-      setAddedTags((prev) => [...prev, tag]);
-    } else {
-      setAddedTags((prev) => prev.filter((t) => t !== tag));
-    }
-  };
-
   return (
-    <TagAdder tags={tags} setTags={setTags} defaultTags={defaultTags} />
+    <TagAdder title={'Add Tags'} tags={tags} setTags={setTags} defaultTags={defaultTags} friendId={params.id} />
   )
 }
--- components/screens/EditTags.tsx
+++ components/screens/EditTags.tsx
@@ -1,45 +1,15 @@
-import { useLocalSearchParams, useRouter } from "expo-router";
-import React, { useEffect, useRef, useState } from "react";
-import {
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import ToastManager, { Toast } from "toastify-react-native";
-import { colors } from "../../constants/Theme";
+import { useLocalSearchParams } from "expo-router";
+import React, { useEffect, useState } from "react";
 import * as friendsService from "../../utilities/friends-service";
 import * as tagsService from "../../utilities/tags-service";
-import TitleBack from "../TitleBack";
-import { useUser } from "../providers/UserContext";
-import { capitalizeFirstLetter } from "../../utilities/helpers";
-import { FontAwesome } from "@expo/vector-icons";
 
 import TagAdder from "../TagAdder";
 
 export default function EditTags() {
   const params = useLocalSearchParams();
-  const [loading, setLoading] = useState(false);
-  const [searchTag, setSearchTag] = useState("");
-  const [addedTags, setAddedTags] = useState([]);
-  const [friend, setFriend] = useState(null);
-  const [userTags, setUserTags] = useState([]);
 
   const [tags, setTags] = useState([]);
   const [defaultTags, setDefaultTags] = useState([]);
-
-
-  const router = useRouter();
-  const scrollViewRef = useRef<ScrollView>();
-  const { user, fetchFriend } = useUser();
-  const { id } = useLocalSearchParams();
-  const searchTagLower = searchTag.toLowerCase();
-  const includesTag = addedTags.some(
-    (tag) => tag.toLowerCase() === searchTagLower
-  );
 
   const fetchTags = async () => {
     const friendData = await friendsService.retrieveFriend(params.id);
@@ -47,97 +17,14 @@ export default function EditTags() {
 
     const tagData = await tagsService.getTags();
     setDefaultTags(tagData);
-
-
-    setAddedTags(
-      friendData.tags.map((tag) => capitalizeFirstLetter(tag.title))
-    );
-    setUserTags(
-      friendData.tags.map((tag) => ({
-        id: tag._id,
-        tag: capitalizeFirstLetter(tag.title),
-      }))
-    );
-    setFriend(friendData);
   };
 
   useEffect(() => {
     fetchTags();
   }, []);
 
-  const handleTagPress = (tag) => {
-    if (!addedTags.includes(tag)) {
-      setAddedTags((prev) => [...prev, tag]);
-    } else {
-      setAddedTags((prev) => prev.filter((t) => t !== tag));
-    }
-  };
-
-  const handleSearchSubmit = () => {
-    if (!addedTags.includes(searchTag) && searchTag !== "") {
-      setAddedTags((prev) => [...prev, searchTag]);
-      setSearchTag("");
-    }
-  };
-
-  function scrollViewSizeChanged(height) {
-    // y since we want to scroll vertically, use x and the width-value if you want to scroll horizontally
-    scrollViewRef.current?.scrollTo({ y: height, animated: true });
-  }
-
-  const handleSubmit = async () => {
-    setLoading(true);
-    console.log("---------------------");
-    const tagsToRemove = userTags.filter((obj) => !addedTags.includes(obj.tag));
-    const sortAdd = userTags.map((obj) => obj.tag);
-    console.log(sortAdd, "SORT ADD");
-    const tagsToAdd = addedTags.filter((tag) => !sortAdd.includes(tag));
-    console.log(tagsToRemove, "TAGS TO REMOVE");
-    console.log(tagsToAdd, "TAGS TO ADD");
-    console.log(`---------------------`);
-
-    try {
-      // Map to functions that return the addTag promise
-      const addTagPromises = tagsToAdd.map(
-        (tag) => () =>
-          tagsService.addTag(friend._id, {
-            title: tag,
-            category: "Popular",
-          })
-      );
-
-      // Map to functions that return the removeTag promise
-      const removeTagPromises = tagsToRemove.map(
-        (tag) => () => tagsService.removeTag(friend._id, tag.id)
-      );
-
-      console.log("RIGHT AFTER REMOVE TAG PROM");
-
-      if (tagsToAdd.length === 0 && tagsToRemove.length > 0) {
-        console.log("JUST REMOVE TAGS");
-        await Promise.all(removeTagPromises.map((remove) => remove()));
-      } else if (tagsToAdd.length > 0 && tagsToRemove.length === 0) {
-        console.log("JUST ADD TAGS");
-        await Promise.all(addTagPromises.map((add) => add()));
-      } else if (tagsToAdd.length > 0 && tagsToRemove.length > 0) {
-        console.log("ADD AND REMOVE TAGS");
-        await Promise.all([
-          ...addTagPromises.map((add) => add()),
-          ...removeTagPromises.map((remove) => remove()),
-        ]);
-      }
-
-      fetchFriend();
-      showToasts();
-    } catch (err) {
-      console.log("ERROR", err);
-      setLoading(false);
-    } finally {
-      setLoading(false);
-    }
-  };
 
   return (
-    <TagAdder tags={tags} setTags={setTags} defaultTags={defaultTags} />
+    <TagAdder title={'Edit Tags'} tags={tags} setTags={setTags} defaultTags={defaultTags} friendId={params.id} />
   );
 }
