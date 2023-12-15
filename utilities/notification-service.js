5adcba44d79950613c05b82a1720faa10e931923
Amber Taveras <taveras.amber@gmail.com>
2023-12-15 03:40:13 -0500

    adds get notif service

--- utilities/notification-service.js
+++ utilities/notification-service.js
@@ -1,10 +1,14 @@
 import sendRequest from "./send-request";
-
-const BASE_URL = "http://localhost:3010/api/device";
+import { WEB_BASE_URL } from "./constants";
 
 export async function acceptNotifications(tokenObj) {
-  const res = await sendRequest(BASE_URL, "POST", tokenObj);
+  const res = await sendRequest(WEB_BASE_URL + '/device', "POST", tokenObj);
 
   console.log("res", res);
   return res;
+}
+
+export async function getNotifications () {
+    const response = await sendRequest(WEB_BASE_URL + '/notifications', 'GET', null);
+    return response;
 }
